a26ecc41a3445213fb19bae8d3419179c49e2706
Mateusz Nowakowski <mat13mn@gmail.com>
2025-07-02 13:42:33 +0200

    remove commented code

--- turnkey/src/features/ui/login.tsx
+++ turnkey/src/features/ui/login.tsx
@@ -96,35 +96,6 @@ export const Login = () => {
               isPending={isPending}
               isUserLoading={isUserLoading}
             />
-            {/* <TooltipProvider delayDuration={200}>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <button
-                  onClick={() => login()}
-                  className="h-20 py-[30px] px-10 text-base bg-brand-orange rounded-full text-black"
-                >
-                  {isLoginPending || isUserLoading
-                    ? "Logging in..."
-                    : "Log in with Passkey"}
-                </button>
-              </TooltipTrigger>
-
-              <TooltipContent>
-                <p className="flex flex-col">
-                  <span>Log in with your previously created Passkey.</span>
-                  <span className="text-xs">Prefixed with [MAIN]</span>
-                </p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-
-          <button
-            onClick={() => createSubOrg()}
-            className="bg-transparent border-2 border-surface-level-4 text-white disabled:text-surface-level-4 py-4 px-10 rounded-full"
-            disabled={isPending}
-          >
-            {isPending ? "Signing up..." : "Signup"}
-          </button> */}
           </div>
         </ClientOnly>
 
