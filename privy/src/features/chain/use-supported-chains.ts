b559bff233448e04eb6a946523345ad9f4b0ed70
Mateusz Nowakowski <mat13mn@gmail.com>
2025-07-03 10:06:34 +0200

    remove privy

--- privy/src/features/chain/use-supported-chains.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchSupportedChains } from "./fetch-supported-chains";
-
-export const useSupportedChains = () => {
-  return useQuery({
-    queryKey: ["supported-chains"],
-    queryFn: fetchSupportedChains,
-  });
-};
--- privy/src/features/onebalance-account/onebalance-account.tsx
+++
@@ -1,8 +0,0 @@
-import { OneBalanceAccountUI } from "../ui/onebalance-account-ui";
-import { useOneBalanceAccountAddress } from "./use-onebalance-account";
-
-export const OneBalanceAccount = () => {
-  const oneBalanceAccountQuery = useOneBalanceAccountAddress();
-
-  return <OneBalanceAccountUI query={oneBalanceAccountQuery} />;
-};
